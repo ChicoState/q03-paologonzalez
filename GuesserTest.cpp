c06da0003146b5dd54132ca5bb28e30f8a93b069
Paolo Gonzalez <paoloag133@gmail.com>
2025-05-05 10:59:58 -0700

    submissiontest

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -20,3 +20,84 @@ TEST(GuesserTest, smoke_test)
   Guesser object("Secret");
   ASSERT_EQ( 1+1, 2 );
 }
+
+TEST(GuesserTest, four_correct)
+{
+  Guesser object("aabb");
+  ASSERT_EQ( true, object.match("aabb") );
+  ASSERT_EQ( true, object.match("aabb") );
+  ASSERT_EQ( true, object.match("aabb") );
+  ASSERT_EQ( true, object.match("aabb") );
+}
+
+TEST(GuesserTest, correct_of1_correct_correct)
+{
+  Guesser object("aabb");
+  ASSERT_EQ( true, object.match("aabb") );
+  ASSERT_EQ( false, object.match("abbb") );
+  ASSERT_EQ( true, object.match("aabb") );
+  ASSERT_EQ( true, object.match("aabb") );
+}
+
+TEST(GuesserTest, correct_off2_correct_correct)
+{
+  Guesser object("aabb");
+  ASSERT_EQ( true, object.match("aabb") );
+  ASSERT_EQ( false, object.match("bbbb") );
+  ASSERT_EQ( true, object.match("aabb") );
+  ASSERT_EQ( true, object.match("aabb") );
+}
+
+TEST(GuesserTest, correct_off3_correct_correct)
+{
+  Guesser object("aabb");
+  ASSERT_EQ( true, object.match("aabb") );
+  ASSERT_EQ( false, object.match("bbab") );
+  ASSERT_EQ( false, object.match("aabb") );
+  ASSERT_EQ( false, object.match("aabb") );
+}
+
+TEST(GuesserTest, correct_off4_correct_correct)
+{
+  Guesser object("aabb");
+  ASSERT_EQ( true, object.match("aabb") );
+  ASSERT_EQ( false, object.match("bbab") );
+  ASSERT_EQ( false, object.match("aabb") );
+  ASSERT_EQ( false, object.match("aabb") );
+}
+
+TEST(GuesserTest, off1_off1_off1_correct)
+{
+  Guesser object("aabb");
+  ASSERT_EQ( false, object.match("abbb") );
+  ASSERT_EQ( false, object.match("abbb") );
+  ASSERT_EQ( false, object.match("abbb") );
+  ASSERT_EQ( false, object.match("aabb") );
+}
+
+TEST(GuesserTest, off1_off3_correct_correct)
+{
+  Guesser object("aabb");
+  ASSERT_EQ( false, object.match("abbb") );
+  ASSERT_EQ( false, object.match("bbab") );
+  ASSERT_EQ( false, object.match("aabb") );
+  ASSERT_EQ( false, object.match("aabb") );
+}
+
+TEST(GuesserTest, off1_off1_correct_off1_correct)
+{
+  Guesser object("aabb");
+  ASSERT_EQ( false, object.match("abbb") );
+  ASSERT_EQ( false, object.match("abbb") );
+  ASSERT_EQ( true, object.match("aabb") );
+  ASSERT_EQ( false, object.match("abbb") );
+  ASSERT_EQ( false, object.match("aabb") );
+}
+
+TEST(GuesserTest, test_empty_string)
+{
+  Guesser object("");
+  ASSERT_EQ( true, object.match("") );
+}
+
+
